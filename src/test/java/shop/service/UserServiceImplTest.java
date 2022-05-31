a2e89100af64031bf3593b7b16f7b880a7e1a61a
kakao <kakao1302@naver.com>
2022-05-31 17:30:35 +0900

    mysql db

--- src/test/java/shop/service/UserServiceImplTest.java
+++ src/test/java/shop/service/UserServiceImplTest.java
@@ -1,15 +1,29 @@
 package shop.service;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import shop.config.SecurityConfig;
 import shop.domain.UserDomain;
+import shop.dto.RegisterRequest;
+import shop.exception.EmailNotFoundException;
 import shop.jpa.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
@@ -24,11 +38,17 @@ class UserServiceImplTest {
     @Autowired
     UserServiceImpl userService;
 
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
     @Test
-    void loadUserByUsername() {
+    @DisplayName("조회")
+    void loadUserByUsername(){
+
     }
 
     @Test
+    @DisplayName("저장 후 조회회")
     void save() {
         //given
         Mockito.when(userRepository.findByEmail("kakao1302@naver.com"))
