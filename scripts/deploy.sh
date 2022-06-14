cb627fda05fec8c60d621d6bfb4812c20259b2f7
kakao <kakao1302@naver.com>
2022-06-14 23:05:34 +0900

    cicd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@ REPOSITORY=/home/ec2-user/app
 PROJECT_NAME=springboot-service
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.war $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep war | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
@@ -19,14 +19,14 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar|grep jar|tail -n 1)
+WAR_NAME=$(ls -tr $REPOSITORY/*.war|grep war|tail -n 1)
 
-echo "> Jar Name: $JAR_NAME"
-echo "> $JAR_NAME에 실행권한 추가"
-chmod +x $JAR_NAME
+echo "> War Name: $WAR_NAME"
+echo "> $WAR_NAME에 실행권한 추가"
+chmod +x $WAR_NAME
 
-echo "> $JAR_NAME 실행"
-nohup java -jar \\
+echo "> $WAR_NAME 실행"
+nohup java -war \\
         -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \\
         -Dspring.profiles.active=real \\
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        $WAR_NAME > $REPOSITORY/nohup.out 2>&1 &
