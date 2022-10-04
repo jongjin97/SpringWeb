0f0f0f2c7e1a9448d29e4d16a9587d417c5f4223
kakao <kakao1302@naver.com>
2022-10-04 21:19:37 +0900

    user test

--- src/test/java/shop/controller/UserControllerTest.java
+++ src/test/java/shop/controller/UserControllerTest.java
@@ -9,11 +9,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import shop.dto.RegisterRequest;
+import shop.dto.User;
 import shop.service.UserService;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -28,13 +32,17 @@ class UserControllerTest {
 
     @Test
     public void 로그인페이지_접속() throws Exception {
-        mockMvc.perform(get("/user/login")).andExpect(status().isOk()).andDo(print());
+        mockMvc.perform(get("/user/login"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("LoginUser"))
+                .andDo(print());
     }
 
     @Test
     public void 회원가입페이지_접속() throws Exception{
-        mockMvc.perform(get("/user/register")).andExpect(status().isOk()).andDo(print());
+        mockMvc.perform(get("/user/register"))
+                .andExpect(model().attributeExists("registerRequest"))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
-
-
 }
