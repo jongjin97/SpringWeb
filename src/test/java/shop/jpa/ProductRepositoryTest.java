40e36e5bca9341aa9344f856529c49addd10b53e
kakao <kakao1302@naver.com>
2022-10-11 21:31:14 +0900

    ProductRepository Test

---
+++ src/test/java/shop/jpa/ProductRepositoryTest.java
@@ -0,0 +1,82 @@
+package shop.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import shop.dto.Product;
+import shop.entity.ProductDomain;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @Transactional
+    void save(){
+        ProductDomain productDomain = getProductDomain();
+
+        ProductDomain savedProduct = productRepository.save(productDomain);
+        assertNotNull(savedProduct);
+    }
+
+    @Test
+    @Transactional
+    void findAllByCategory() {
+        ProductDomain productDomain = getProductDomain();
+
+        productRepository.save(getProductDomain());
+        productRepository.save(getProductDomain());
+        List<ProductDomain> productDomainList = productRepository.findAllByCategory(productDomain.getCategory());
+
+        assertEquals(productDomainList.size(), 2);
+    }
+
+    @Test
+    @Transactional
+    void findByName() {
+        ProductDomain productDomain = getProductDomain();
+
+        productRepository.save(productDomain);
+        ProductDomain resultProduct = productRepository.findByName(productDomain.getName());
+
+        assertEquals(productDomain.getName(), resultProduct.getName());
+    }
+
+    @Test
+    @Transactional
+    void removeByName() {
+        ProductDomain productDomain = getProductDomain();
+
+        ProductDomain savedProduct = productRepository.save(productDomain);
+        assertNotNull(savedProduct);
+
+        productRepository.removeByName(productDomain.getName());
+        ProductDomain findProduct = productRepository.findByName(productDomain.getName());
+
+        assertNull(findProduct);
+    }
+
+    ProductDomain getProductDomain(){
+        Product product = Product.builder()
+                .name("name")
+                .category("category")
+                .file_name("file_name")
+                .file_path("file_path")
+                .price(10000)
+                .content("content")
+                .qty(1)
+                .build();
+        ProductDomain productDomain = new ProductDomain(product);
+        return  productDomain;
+    }
+}
